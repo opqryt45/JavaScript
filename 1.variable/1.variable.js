d2635deb35201abb8e1477acc213c9c96a41ab48
희산의 MacBook Air <heesankim@192.168.nate.com>
2022-07-09 13:59:51 +0900

    boolean add

--- 1.variable/1.variable.js
+++ 1.variable/1.variable.js
@@ -2,6 +2,8 @@
 // 자료를 저장할 수 있는 이름이 주어진 기억장소!
 let a = 0; // a는 변수이름.   = 은 할당연산자.  1은 값. ; 문의끝 
 console.log(a);
+console.log(!!a);
+console.log(Boolean(a));
 
 a = 2;
 console.log(a);
@@ -9,6 +11,8 @@ console.log(a);
 let b;
 console.log(b);
 
-b = 3;   //재할당
+b = 333;   //재할당
 console.log(b);
+console.log(Boolean(b));
+
 
